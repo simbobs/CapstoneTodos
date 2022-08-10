a1b32ebbc4abaf559ef61e28ad547606993372ed
cameronjwils <103742332+cameronjwils@users.noreply.github.com>
2022-08-10 21:46:44 +0100

    about section updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,13 +9,36 @@ const About = () => {
         </div>
         <div className="text-body">
         <p>
-          The Todos Project is deisigned so that we reimagine the way we think about adventure, exploration and learning as a collective.
+          The Todos Project is deisigned so that we reimagine the way we think about adventure, 
+          exploration and learning as a collective.
         </p>
         </div>
         <div className="text-body">
         <p>
-          We have built an App that shows Scotland off in a modern and interactive directive.
+          We have built an App that shows Scotland off in a modern and interactive directive. 
+          Our app allows carers to tarck and view attractions all over Scotland to see the 
+          accessability and information of each attraction.   
         </p>
+        </div>
+        <div className="dev-text">
+          <h2>
+            Developers
+          </h2>
+        <li>
+        Cameron Wilson: https://github.com/cameronjwils
+        </li>
+        <li>
+        Simona Demarco: https://github.com/simbobs
+        </li>
+        <li>
+        Jonny Houdin-McAveety: https://github.com/jonnyhoudini
+        </li>
+        <li>
+        Devan McClure: https://github.com/DevMcClure
+        </li>
+        <li>
+        Louise Archibald: https://github.com/louarchibald
+        </li>
         </div>
     </>
   )
