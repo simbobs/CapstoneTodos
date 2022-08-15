faca1462f72b274b790619aef9f79c6e0f09e5f0
simbobs <simonapiademarco@gmail.com>
2022-08-15 18:22:38 +0100

    this main container works

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Filter from '../components/Filter';
+import Filter from '../components/filterComponents/Filter';
 import AttractionDetail from '../components/AttractionDetail';
-import AttractionList from './AttractionList';
 
 const MainContainer = ({comments, user, addNewComment, locations, attractions, changeSelectedAttraction, addToUserFavourites, filtered, filter, selectedAttraction, updateAttraction, removeAttraction, goBackToList}) => {
 
@@ -10,7 +9,7 @@ const MainContainer = ({comments, user, addNewComment, locations, attractions, c
         <>
 
         {selectedAttraction ? <AttractionDetail attraction={selectedAttraction} locations={locations} removeAttraction={removeAttraction} goBackToList={goBackToList} updateAttraction={updateAttraction} comments={comments} user={user} addNewComment={addNewComment} /> : 
-          <Filter locations={locations} attractions={attractions} filtered={filtered} filter={createFilteredList} changeSelectedAttraction={changeSelectedAttraction} addToUserFavourites={addToUserFavourites} goBackToList={goBackToList} />}
+          <Filter locations={locations} attractions={attractions} filtered={filtered} filter={filter} changeSelectedAttraction={changeSelectedAttraction} addToUserFavourites={addToUserFavourites} goBackToList={goBackToList} />}
 
         </>  
     )
