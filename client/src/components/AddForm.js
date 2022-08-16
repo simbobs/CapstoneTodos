6387f453b54fcf867d92b244e736a500d1beb185
louarchibald <103283560+louarchibald@users.noreply.github.com>
2022-08-16 13:12:30 +0100

    adding missing stylesheet

--- client/src/components/AddForm.js
+++ client/src/components/AddForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { postAttraction } from '../services/services.js'
-import "../static/Form.css"
+import "../static/form.css"
 import { useNavigate } from 'react-router-dom';
 
 
